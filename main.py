31315065248b880cf131731e4165cbf9e314b2e9
User <csurucean@gmail.com>
2022-09-27 19:19:15 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("hello World")
+"""print("hello World")
 print("hello \" World")
 
 Var = 5
@@ -9,4 +9,16 @@ print(2 - 1)
 print(2 * 3)
 print(2 / 4)
 print(2 ** 3)
+
+a = 1
+b = 1.3
+c = "Hola"
+c = 4/2
+print(c)
+print(type(c))
+"""
+
+c = 'Hello'
+B = 'Sam'
+print(c + B)
 
