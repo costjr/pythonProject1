20e814426711421828b6edcb17e41051d0f3156a
User <csurucean@gmail.com>
2022-09-27 20:24:04 +0300

    Initial commit

--- main.py
+++ main.py
@@ -16,9 +16,59 @@ c = "Hola"
 c = 4/2
 print(c)
 print(type(c))
-"""
 
 c = 'Hello'
 B = 'Sam'
 print(c + B)
 
+
+
+#a = int(input("your number"))
+#print(a * 3)
+
+
+a = int(input("your number"))
+b = 4
+print(a * b)
+
+name = input("Your name:")
+print("hello, " + name)
+
+
+float_var = 3.14
+print(int(float_var))
+print(str(float_var))
+print(bool(float_var))
+
+bool_var = True
+print(int(bool_var))
+
+hour = 14
+
+if hour < 12:
+    print("good morning")
+elif hour < 18:
+    print("good evening")
+else:
+    print("Hello")
+
+
+op = 4
+
+match op:
+    case 1:
+        print("op is 1")
+    case 2:
+        print("op is 2")
+    case _:
+        print("No such Value")
+
+age = 18
+
+if age > 18:
+    print("Allowed")
+elif age == 18:
+    print("Allowed")
+else:
+    print("Not Allowwd")
+"""
