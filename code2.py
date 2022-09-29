e59d368805728ea1d7f796a011bccb7e73f0c12a
User <csurucean@gmail.com>
2022-09-29 19:10:41 +0300

    Initial commit

---
+++ code2.py
@@ -0,0 +1,39 @@
+"""a = 1
+while a < 18:
+    print('You are' + str(a) + "years old")
+    a += 1
+print('Happy BDay')
+
+
+for c in range(1, 18):
+    print('You have not reached yet 18')
+    print("Recently you made ", c,"years old")
+    print('Until reaching 18 you need', 18 - c, "years")
+
+
+a = 1
+while a < 3:
+    print(a)
+
+
+for b in range(1, 13):
+    b += 1
+    print(b)
+
+
+
+a = 5
+b = 3.6
+print(a + b)
+print(a - b)
+print(a * b)
+print(a ** b)
+print(a / b)
+print(b % a)
+"""
+
+a = 8
+b = -3
+a,b = b,a
+print(a)
+print(b)
