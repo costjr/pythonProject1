d10d9c9520655b4deddddb3127ea09518dc673b1
User <csurucean@gmail.com>
2022-10-04 19:53:48 +0300

    Initial commit

--- code3.py
+++ code3.py
@@ -9,3 +9,64 @@ while 1:
         break
     else:
         print("Mai incearca")"""
+
+
+"""test = []
+l = int(input('Enter the array lenght'))
+for i in range(0,l):
+    test.append(int(input('Enter element with index' + str(i) + '\n')))"""
+
+
+"""a = [3, 4, 5, 6, 7, 8, 9, 23, 45]
+
+a.append(2)
+print(a)
+
+b = a.copy()
+print(a, b)
+
+c = a.count(3)
+print(c)
+
+b.extend(a)
+print(b, a)
+
+print(a.index(45))
+
+a.insert(8, 'Sui')
+print(a)
+
+print(a.pop(8))
+print(a)
+
+a.remove(3)
+print(a)
+
+a.reverse()
+print(a)
+
+a.sort()
+print(a)
+
+a.clear()
+print(a)"""
+
+
+"""tuple_ex = ('hola', 5, 'Test', 4.55, False)
+print(tuple_ex[0])
+print(tuple_ex + ('New', 'data', 1))"""
+
+
+"""simple_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+set_array_2 = {4, 3, 2, 4, 5, 5, 41, 2}
+set_array = set(simple_array)
+print(simple_array)
+print(set_array)
+print(set_array_2)"""
+
+
+dictionary = {'Key' : 'Value', "Int": 3, "Float": 23.4, "Bool": True}
+print(dictionary.get('Key'))
+dictionary.update({'Int': 4})
+dictionary['Key'] = "New value"
+print(dictionary)
