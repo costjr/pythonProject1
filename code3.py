278096be7c19470a44514c5a6001d80853f16d41
User <csurucean@gmail.com>
2022-10-04 18:47:32 +0300

    Initial commit

---
+++ code3.py
@@ -0,0 +1,11 @@
+litera = "q"
+user = ""
+
+while 1:
+    user = input("Introduceti o litera")
+
+    if litera == user:
+        print("Felicitari")
+        break
+    else:
+        print("Mai incearca")
