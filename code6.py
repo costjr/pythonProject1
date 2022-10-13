30b015781b9d09193ed24455baca5a055c61a985
User <csurucean@gmail.com>
2022-10-13 19:16:03 +0300

    Initial commit

---
+++ code6.py
@@ -0,0 +1,19 @@
+"""f = open("sui.txt", "r")
+#print("nor\noc"); print(nor\toc)
+#print(f.read(5))
+#print(f.read(5))
+#print(f.read(5))
+
+lines = f.readlines()
+print(lines)
+for line in lines:
+    print(line)
+f.close()"""
+
+f = open("sui.txt", "a")#"w"
+f.write("\nfgfagdfsagfdta")
+f.close()
+#first part of the code adds the text to your text file
+f = open("sui.txt")
+print(f.read())
+#putting another name to the file can while create automaticly a new text file
