8cf042e69f45741e59e764dfa039a42b44bda2b6
User <csurucean@gmail.com>
2022-11-03 20:30:43 +0200

    Initial commit

--- lesson10/main1.py
+++ lesson10/main1.py
@@ -6,28 +6,32 @@ from datetime import datetime
 from kivymd.uix.list import TwoLineAvatarIconListItem, ILeftBodyTouch
 from kivymd.uix.selectioncontrol import MDCheckbox
 from database import Database
-
+# Initialize db instance
 db = Database()
 class DialogContent(MDBoxLayout):
-
+    """OPENS A DIALOG BOX THAT GETS THE TASK FROM THE USER"""
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.ids.date_text.text = str(datetime.now().strftime('%A %d %B %Y'))
     def show_date_picker(self):
-
+        """Opens the date picker"""
         date_dialog = MDDatePicker()
         date_dialog.bind(on_save=self.on_save)
         date_dialog.open()
     def on_save(self, instance, value, date_range):
-
+        """This functions gets the date from the date picker and converts its it a
+        more friendly form then changes the date label on the dialog to that"""
         date = value.strftime('%A %d %B %Y')
         self.ids.date_text.text = str(date)
 class ListItemWithCheckbox(TwoLineAvatarIconListItem):
-
+    '''Custom list item'''
     def __init__(self, pk=None, **kwargs):
         super().__init__(**kwargs)
+        # state a pk which we shall use link the list items with the database
+        # primary keys
         self.pk = pk
     def mark(self, check, the_list_item):
+        '''mark the task as complete or incomplete'''
         if check.active == True:
             the_list_item.text = '[s]'+the_list_item.text+'[/s]'
             db.mark_task_as_complete(the_list_item.pk)# here
@@ -35,13 +39,15 @@ class ListItemWithCheckbox(TwoLineAvatarIconListItem):
             the_list_item.text = str(db.mark_task_as_incomplete(the_list_item.pk))# Here
 
     def delete_item(self, the_list_item):
+        '''Delete the task'''
         self.parent.remove_widget(the_list_item)
         db.delete_task(the_list_item.pk)# Here
 class LeftCheckbox(ILeftBodyTouch, MDCheckbox):
-    pass
+    '''Custom left container'''
 class MainApp(MDApp):
     task_list_dialog = None
     def build(self):
+        # Setting theme to my favorite theme
         self.theme_cls.primary_palette = "Gray"
     def show_task_dialog(self):
         if not self.task_list_dialog:
@@ -49,6 +55,7 @@ class MainApp(MDApp):
                 title="Create Task",type="custom",content_cls=DialogContent(), )
         self.task_list_dialog.open()
     def on_start(self):
+        """Load the saved tasks and add them to the MDList widget when the application starts"""
         try:
             completed_tasks, uncomplete_tasks = db.get_tasks()
             if uncomplete_tasks != []:
@@ -69,8 +76,10 @@ class MainApp(MDApp):
     def close_dialog(self, *args):
         self.task_list_dialog.dismiss()
     def add_task(self, task, task_date):
-
-        created_task = db.create_task(task.text, task_date)
+        '''Add task to the list of tasks'''
+        # Add task to the db
+        created_task = db.create_task(task.text, task_date)# Here
+        # return the created task details and create a list item
         self.root.ids['container'].add_widget(ListItemWithCheckbox(pk=created_task[0],
                     text='[b]'+created_task[1]+'[/b]', secondary_text=created_task[2]))
         task.text = ''
