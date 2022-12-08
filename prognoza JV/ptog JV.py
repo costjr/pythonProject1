a87e335da4d94c4554f2ee5981254922bf6e2cb2
User <csurucean@gmail.com>
2022-12-08 20:14:42 +0200

    Initial commit

---
+++ prognoza JV/ptog JV.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+data = pd.read_csv("https://gist.githubusercontent.com/zhonglism/f146a9423e2c975de8d03c26451f841e/raw/f79e190df4225caed58bf360d8e20a9fa872b4ac/vgsales.csv")
+print(data.head())
+
+print(data.isnull().sum())
+data = data.dropna()
+
+game = data.groupby("Genre")["Global_Sales"].count().head(10)
+custom_colors = mpl.colors.Normalize(vmin=min(game), vmax=max(game))
+colours = [mpl.cm.PuBu(custom_colors(i)) for i in game]
+plt.figure(figsize=(7,7))
+plt.pie(game, labels=game.index, colors=colours)
+central_circle = plt.Circle((0, 0), 0.5, color='white')
+fig = plt.gcf()
+fig.gca().add_artist(central_circle)
+plt.rc('font', size=12)
+plt.title("Top 10 Categories of Games Sold", fontsize=20)
+plt.show()
+
+x = data[["Rank", "NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales"]]
+y = data("Global_Sales")
+
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(xtrain, ytrain)
+predictions = model.predict(xtest)
